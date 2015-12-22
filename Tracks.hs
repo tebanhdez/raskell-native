29b05fb2d8db80686c9acc7dfa49b4d30efb3387
Kevin Escobar Miranda <tec.201265615@gmail.com>
2015-12-22 14:25:58 -0600

    TrackAPI test

--- Tracks.hs
+++ Tracks.hs
@@ -1,5 +1,21 @@
 module Tracks where
 
+
+import Control.Applicative
+import Control.Concurrent.STM
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Hourglass
+import Data.Proxy
+import Data.Text
+import GHC.Generics
+import Network.Wai.Handler.Warp
+import Servant
+import System.Environment
+import System.Hourglass
+import System.IO
+
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.List as L
@@ -97,3 +113,4 @@ trackServer home tracks =
 
 
 
+    
