c725ff0772c4a01473be9563c4e7078e7b6fe52f
Kevin Escobar Miranda <tec.201265615@gmail.com>
2015-12-22 16:54:59 -0600

    TrackAPI test

--- Main.hs
+++ Main.hs
@@ -1,36 +1,72 @@
-import Control.Applicative
-import Control.Concurrent.STM
-import Control.Monad
-import Control.Monad.IO.Class
-import Data.Aeson
-import Data.Hourglass
-import Data.Proxy
-import Data.Text
-import GHC.Generics
-import Network.Wai.Handler.Warp
-import Servant
-import System.Environment
-import System.Hourglass
-import System.IO
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds         #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
 
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import qualified Data.List as L
+import           Data.Aeson
+import           Data.Monoid
+import           Data.Proxy
+import           Data.Text
+import           GHC.Generics
+import           Network.Wai
+import           Network.Wai.Handler.Warp
 
-import Tracks
-import Notes
-import Songs
+import           Servant
 
+-- * Example
+
+-- | A greet message data type
+newtype Greet = Greet { _msg :: Text }
+  deriving (Generic, Show)
+
+instance FromJSON Greet
+instance ToJSON Greet
+
+-- API specification
+type TestApi =
+       -- GET /hello/:name?capital={true, false}  returns a Greet as JSON
+       "hello" :> Capture "name" Text :> QueryParam "capital" Bool :> Get '[JSON] Greet
+
+       -- POST /greet with a Greet as JSON in the request body,
+       --             returns a Greet as JSON
+  :<|> "greet" :> ReqBody '[JSON] Greet :> Post '[JSON] Greet
+
+       -- DELETE /greet/:greetid
+  :<|> "greet" :> Capture "greetid" Text :> Delete '[JSON] ()
+
+testApi :: Proxy TestApi
+testApi = Proxy
+
+-- Server-side handlers.
+--
+-- There's one handler per endpoint, which, just like in the type
+-- that represents the API, are glued together using :<|>.
+--
+-- Each handler runs in the 'ExceptT ServantErr IO' monad.
+server :: Server TestApi
+server = helloH :<|> postGreetH :<|> deleteGreetH
+
+  where helloH name Nothing = helloH name (Just False)
+        helloH name (Just False) = return . Greet $ "Hello, " <> name
+        helloH name (Just True) = return . Greet . toUpper $ "Hello, " <> name
+
+        postGreetH greet = return greet
+
+        deleteGreetH _ = return ()
+
+-- Turn the server into a WAI app. 'serve' is provided by servant,
+-- more precisely by the Servant.Server module.
+test :: Application
+test = serve testApi server
+
+-- Run the server.
+--
+-- 'run' comes from Network.Wai.Handler.Warp
+runTestServer :: Port -> IO ()
+runTestServer port = run port test
+
+-- Put this all to work!
 main :: IO ()
-main = do
-    hSetBuffering stdout LineBuffering
-    env <- getEnvironment
-    let port = maybe 8080 read $ lookup "PORT" env
-        home = maybe "Welcome to Haskell on Heroku" T.pack $
-                 lookup "TUTORIAL_HOME" env
-    --notes <- emptyNotes
-    --run port $ serve noteAPI $ server home notes
-    --tracks <- emptyTracks
-    --run port $ serve trackAPI $ trackServer home tracks
-    songs <- emptySongs
-    run port $ serve songAPI $ songServer home songs
+main = runTestServer 8001
