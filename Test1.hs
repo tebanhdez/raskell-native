8e5e675ab3c92f8a87afa0e81062e7130df18914
Kevin Escobar Miranda <tec.201265615@gmail.com>
2015-12-23 10:25:11 -0600

    testing general API

--- Test1.hs
+++ Test1.hs
@@ -1,12 +1,18 @@
 module Test1 where
 
 
+data TestData = TestData
+	{
+		testText :: Text
+	}
+	deriving (Generic, Show)
+instance ToJSON TestData
 
-callMethod1 :: IO (TVar [Song])
+callMethod1 :: IO (TVar [TestData])
 callMethod1 param1 = newTVarIO []
 
 type Test1API = 
-	"test1" :> Get [testData] -- get of testData
+	"test1" :> Get [TestData] -- get of testData
 
 test1Server :: Server Test1API
 test1Server = method1 -- :<|> method2
