d3e9a51a4a525e41c42a61266358aaaa039b1682
Kevin Escobar Miranda <tec.201265615@gmail.com>
2015-12-23 08:58:27 -0600

    APIs separated by modules

---
+++ Notes.hs
@@ -0,0 +1,88 @@
+module Notes where
+
+
+import Control.Applicative
+import Control.Concurrent.STM
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Hourglass
+import Data.Proxy
+import Data.Text
+import GHC.Generics
+import Network.Wai.Handler.Warp
+import Servant
+import System.Environment
+import System.Hourglass
+import System.IO
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.List as L
+
+
+getISO8601DateTime :: IO Text
+getISO8601DateTime = do
+    seconds <- timeCurrent
+    let iso = timePrint ISO8601_DateAndTime seconds
+    return $ T.pack iso
+
+
+data Note = Note
+    { contents :: Text
+    , dateTime :: Text
+    }
+  deriving (Generic, Show)
+
+instance ToJSON Note
+
+
+newtype PostNote = PostNote
+    { postContents :: Text
+    }
+  deriving Show
+
+instance FromJSON PostNote where
+    parseJSON (Object o) = PostNote <$> o .: "contents"
+    parseJSON _          = mzero
+
+
+emptyNotes :: IO (TVar [Note])
+emptyNotes =
+    newTVarIO []
+
+getNotes :: MonadIO m => TVar [Note] -> m [Note]
+getNotes notes =
+    liftIO $ readTVarIO notes
+
+postNote :: MonadIO m => TVar [Note] -> PostNote -> m [Note]
+postNote notes post =
+    liftIO $ do
+      iso <- getISO8601DateTime
+      T.putStrLn $ T.concat [iso, " ", postContents post]
+      let note = Note
+            { contents = postContents post
+            , dateTime = iso
+            }
+      atomically $ do
+        oldNotes <- readTVar notes
+        let newNotes = note : oldNotes
+        writeTVar notes newNotes
+        return newNotes
+
+
+type NoteAPI =
+         Get Text
+    :<|> "notes" :> Get [Note]
+    :<|> "notes" :> ReqBody PostNote :> Post [Note]
+
+noteAPI :: Proxy NoteAPI
+noteAPI =
+    Proxy
+
+server :: Text -> TVar [Note] -> Server NoteAPI
+server home notes =
+         return home
+    :<|> getNotes notes
+    :<|> postNote notes
+
---
+++ Tracks.hs
@@ -0,0 +1,107 @@
+module Tracks where
+
+
+import Control.Applicative
+import Control.Concurrent.STM
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Hourglass
+import Data.Proxy
+import Data.Text
+import GHC.Generics
+import Network.Wai.Handler.Warp
+import Servant
+import System.Environment
+import System.Hourglass
+import System.IO
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.List as L
+
+----
+-- Tracks
+----
+
+getMatches trackId = return [Match {title = "title 1", probability = 0.5},Match {title = "title 2", probability = 0.3}]                     
+
+data Match = Match
+    { 
+    title :: Text,
+    probability :: Float
+    }
+  deriving (Generic, Show)
+instance ToJSON Match
+
+data Performance = Performance
+    { 
+    information :: Text
+    }
+  deriving (Generic, Show)
+instance ToJSON Performance
+
+data Track = Track
+    { 
+    trackId :: Text,
+    requestId :: Text,
+    matches :: [Match],
+    performance :: Performance
+    }
+  deriving (Generic, Show)
+instance ToJSON Track
+
+
+newtype PostTrack = PostTrack
+    { tracksContents :: Text
+    }
+  deriving Show
+
+instance FromJSON PostTrack where
+    parseJSON (Object o) = PostTrack <$> o .: "trackId"
+    parseJSON _          = mzero
+
+
+emptyTracks :: IO (TVar [Track])
+emptyTracks =
+    newTVarIO []
+
+getTracks :: MonadIO m => TVar [Track] -> m [Track]
+getTracks tracks =
+    liftIO $ readTVarIO tracks
+
+postTrack :: MonadIO m => TVar [Track] -> PostTrack -> m [Track]
+postTrack tracks post =
+    liftIO $ do
+      T.putStrLn $ T.concat [tracksContents post]
+      let trackId = tracksContents post
+      let track = Track
+            { 
+              trackId = trackId,
+              requestId = "1234-1234-1234",
+              matches = L.concat (getMatches trackId),
+              performance = Performance {information = "some information here"}
+            }
+      atomically $ do
+        oldTracks <- readTVar tracks
+        let newTracks = track : oldTracks
+        writeTVar tracks newTracks
+        return newTracks
+
+
+type TrackAPI =
+         Get Text
+    :<|> "tracks" :> Get [Track]
+    :<|> "tracks" :> ReqBody PostTrack :> Post [Track]
+
+
+trackAPI :: Proxy TrackAPI
+trackAPI =
+    Proxy
+
+
+trackServer :: Text -> TVar [Track] -> Server TrackAPI
+serverTrack home tracks =
+         return home
+    :<|> getTracks tracks
+    :<|> postTrack tracks
